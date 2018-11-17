e9359acb8c31d19ad0ecb351f412399c378fb704
zhangliang <914342085@qq.com>
2018-11-17 09:40:47 +0800

    完成增加文章

--- public/js/admin/addPost.js
+++ public/js/admin/addPost.js
@@ -6,14 +6,15 @@ $(function(){
         $.post('/api/post/addPost', data, function(result){
             console.log(result);
             if (result.status == 0) {
-                layer.msg(result.message, {icon: 1, time: 5000}, function(){
+                parent.layer.msg(result.message, {icon: 1}, function(){
                     console.log(parent.window.location);
                     let $table = $(parent).find("#postAdminTable");
-                    // let index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
-                    // parent.layer.close(index); //再执行关闭
                 });
-                //let index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
-                //parent.layer.close(index); //再执行关闭
+                //刷新表格
+                parent.$("#postAdminTable").bootstrapTable("refresh");
+                //关闭编辑窗
+                let index = parent.layer.getFrameIndex(window.name); //先得到当前iframe层的索引
+                parent.layer.close(index); //再执行关闭
             }
             else {
                 layer.msg(result.message, {icon: 2});
